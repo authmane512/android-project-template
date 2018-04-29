7ccbcfc25337bee5aec81c5b5e965f6545f80d83
Authmane Terki <authmane512@protonmail.ch>
2018-04-29 18:51:46 +0200

    Version 1.0

--- README.md
+++ README.md
@@ -2,5 +2,8 @@ Hello!
 
 I've made this little script that allow you to init, compile and run an Android Project. I tried to make it as simple as possible to allow you to understand and modify it easily. If you think there is a very important missing feature, don't hesitate to do a pull request on Github and I will answer quickly.
 
+To use it, check this tutorial:
+https://medium.com/@authmane512/how-to-do-android-development-faster-without-gradle-9046b8c1cf68
+
 Thanks!
 
--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # Author: Authmane Terki (authmane512)
 # E-mail: authmane512 (at) protonmail.ch
 # Blog: https://medium.com/@authmane512
-# Source: https://github.com/DroidyTools/android-project-template
+# Source: https://github.com/authmane512/android-project-template
 # This project is on public domain
 #
 # Hello! I've made this little script that allow you to init, compile and run an Android Project. I tried to make it as simple as possible to allow you to understand and modify it easily. If you think there is a very important missing feature, don't hesitate to do a pull request on Github and I will answer quickly. Thanks! 
